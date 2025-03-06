7a39dbecf13b3cb7402336fddb6cad1b8e9ac6de
MicheleValsania <michele.buccino@gmail.com>
2025-03-06 09:21:34 -0500

    user management, login, logout, register

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -1,18 +1,19 @@
-# Uncomment the imports before you add the code
-# from django.urls import path
+# Django imports
+from django.urls import path
 from django.conf.urls.static import static
 from django.conf import settings
-# from . import views
+from . import views
 
 app_name = 'djangoapp'
 urlpatterns = [
-    # # path for registration
+    path(route='login', view=views.login_user, name='login'),
+    # path for home page
+    path('', views.index, name='index'),
+    # path for logout
+    path('logout/', views.logout_user, name='logout'),
+    path('register', views.registration, name='register'),
+]
 
-    # path for login
-    # path(route='login', view=views.login_user, name='login'),
-
-    # path for dealer reviews view
-
-    # path for add a review view
-
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+# Add URL patterns for serving static files
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
